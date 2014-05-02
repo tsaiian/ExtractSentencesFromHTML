17f62287b700fd24885f7cfaecdc67042e01c4e8
Yi Lin Tsai <tsaiian@gmail.com>
2014-05-02 22:58:29 +0800

    Record <H1>-<H2> subtopic information

--- HTMLtoContent/Program.cs
+++ HTMLtoContent/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.IO;
 using System.Net;
 using HtmlAgilityPack;
+using System.Text.RegularExpressions;
 
 namespace HTMLtoContent
 {
@@ -32,18 +33,16 @@ namespace HTMLtoContent
                 HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
                 HtmlNode.ElementsFlags.Remove("form");
                 doc.LoadHtml(html);
-                
-                string strResult = "";
-                if (doc.DocumentNode.SelectSingleNode("//body") != null)
+                HtmlNode bodyNode = doc.DocumentNode.SelectSingleNode("//body");
+
+                TopicBlocks tbs = new TopicBlocks(doc.DocumentNode.SelectSingleNode("//html//head//title"));
+                if (bodyNode != null)
                 {
-                    MainBodyDetector mbd = new MainBodyDetector(doc.DocumentNode.SelectSingleNode("//body"), thresholdT);
-                    strResult = ExtractText(doc.DocumentNode.SelectSingleNode("//body"), mbd);
+                    MainBodyDetector mbd = new MainBodyDetector(bodyNode, thresholdT);
+                    Traversal(bodyNode, mbd, tbs);
                 }
-                else
-                    strResult = "";
 
-
-                PreprocessingAndWrite(@".\Converted\" + di.Name + ".txt", strResult, query);
+                PreprocessingAndWrite(@".\Converted\" + di.Name + ".txt", tbs, query);
 
                 //暫時先處理一個html就好
                 //k++;
@@ -56,57 +55,72 @@ namespace HTMLtoContent
             Console.ReadKey();
 
         }
-        static private string ExtractText(HtmlAgilityPack.HtmlNode node, MainBodyDetector mbd)
+        static private void Traversal(HtmlAgilityPack.HtmlNode node, MainBodyDetector mbd, TopicBlocks tbs)
         {
             if (node.Name.Equals("script") || node.Name.Equals("noscript") || node.Name.Equals("style") || node.Name.Equals("#comment") || !mbd.isMainBody(node))
-            {
-                return "\n";
-            }
+                tbs.addExtractedText("\n");
             else
             {
+                Regex HeadRegex = new Regex("h[1-6]");
+                if (HeadRegex.IsMatch(node.Name))
+                {
+                    //save
+                    tbs.SaveBlock();
+
+                    //change header
+                    int hx = Convert.ToInt16(node.Name.Substring(1));
+                    string subtopic = WebUtility.HtmlDecode(node.InnerText).Replace("\n", "").Replace("\t", "").Replace("\r", "");
+                    tbs.addNewHeader(hx, subtopic);
+                }
+
                 if (node.ChildNodes.Count == 0)
                 {
-                    return WebUtility.HtmlDecode(node.InnerText);
+                    string result = WebUtility.HtmlDecode(node.InnerText);
+                    tbs.addExtractedText(result);
                 }
                 else
                 {
-                    string result = "";
-                    HtmlAgilityPack.HtmlNodeCollection hnc = node.ChildNodes;
-                    foreach (HtmlAgilityPack.HtmlNode n in hnc)
-                    {
-                        result += ExtractText(n, mbd);
-                    }
-                    return result;
+                    HtmlNodeCollection hnc = node.ChildNodes;
+                    foreach (HtmlNode n in hnc)
+                        Traversal(n, mbd, tbs);
                 }
             }
         }
-        static private void PreprocessingAndWrite(string outputFileName, string str, List<string> query)
+        static private void PreprocessingAndWrite(string outputFileName, TopicBlocks tbs, List<string> query)
         {
             StreamWriter sw = new StreamWriter(outputFileName);
 
-            string[] lines = str.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (string line in lines)
+            foreach (Pair<string, string[]> block in tbs.getBlocks())
             {
-                string[] sentences = NLPmethods.SentDetect(line);
-                foreach (string s in sentences)
+                string[] lines = block.first.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string line in lines)
                 {
-                    string afterTrim = s.Trim(new char[] { '\t', ' ' });
-                    string[] tokens = NLPmethods.Stemming(NLPmethods.FilterOutStopWords(NLPmethods.Tokenization(afterTrim)));
-
-                    int tf = 0;
-                    foreach (string token in tokens)
+                    string[] sentences = NLPmethods.SentDetect(line);
+                    foreach (string s in sentences)
                     {
-                        if (query.Contains(token))
-                            tf++;
-                    }
+                        string afterTrim = s.Trim(new char[] { '\t', ' ' });
+                        string[] tokens = NLPmethods.Stemming(NLPmethods.FilterOutStopWords(NLPmethods.Tokenization(afterTrim)));
 
-                    if (tokens.Length >= 3 && tf >= 1)
-                        sw.WriteLine(tf + "\t" + afterTrim);
+                        int tf = 0;
+                        foreach (string token in tokens)
+                        {
+                            if (query.Contains(token))
+                                tf++;
+                        }
+
+                        if (tokens.Length >= 3 && tf >= 1)
+                        {
+                            sw.Write("Subtopic:\t");
+                            for (int i = 0; i < block.second.Length; i++)
+                                sw.Write(block.second[i] + "\t");
+
+                            sw.WriteLine();
+                            sw.WriteLine(tf + "\t" + afterTrim);
+                        }
+                    }
                 }
             }
             sw.Close();
         }
-
-
     }
 }
---
+++ HTMLtoContent/TopicBlocks.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace HTMLtoContent
+{
+    class TopicBlocks
+    {
+        private List<int> beginIndex = null;
+        private List<int> endIndex = null;
+        private List<List<string>> subtopicList = null;
+
+        private List<string> subtopicNow = null;
+        private int start = 0, now = 0;
+        private string fullText = String.Empty;
+
+        public TopicBlocks(HtmlNode titleNode)
+        {
+            beginIndex = new List<int>();
+            endIndex = new List<int>();
+            subtopicList = new List<List<string>>();
+            subtopicNow = new List<string>();
+            start = now = 0;
+
+            if (titleNode == null)
+                subtopicNow.Add(String.Empty);
+            else
+                subtopicNow.Add(titleNode.InnerText);
+        }
+
+        public void SaveBlock()
+        {
+            beginIndex.Add(start);
+            endIndex.Add(now);
+            subtopicList.Add(subtopicNow);
+            start = now;
+
+            subtopicNow = new List<string>(subtopicNow);
+        }
+
+        public void addNewHeader(int headerLevel, string subtopic)
+        {
+            if (subtopicNow.Count - 1 > headerLevel)
+            {
+                for (int i = subtopicNow.Count - 1; i >= headerLevel; i--)
+                    subtopicNow.RemoveAt(i);
+            }
+            else if (subtopicNow.Count - 1 < headerLevel)
+            {
+                for (int i = subtopicNow.Count - 1; i < headerLevel - 1; i++)
+                    subtopicNow.Add(String.Empty);
+            }
+            else
+                subtopicNow.RemoveAt(headerLevel);
+
+            subtopicNow.Add(subtopic);
+        }
+
+        public void addExtractedText(string s)
+        {
+            now += s.Length;
+            fullText += s;
+        }
+
+        public Pair<string, string[]>[] getBlocks()
+        {
+            List<Pair<string, string[]>> blocksWithSubtopic = new List<Pair<string,string[]>>();
+
+            if (fullText.Equals(String.Empty))
+                return blocksWithSubtopic.ToArray();
+
+            try
+            {
+                for (int i = 0; i < subtopicList.Count; i++)
+                {
+                    string block = fullText.Substring(beginIndex[i], endIndex[i] - beginIndex[i] - 1);
+
+                    List<string> simplifySubtopicList = new List<string>();
+                    foreach (string title in subtopicList[i])
+                        if (!title.Equals(String.Empty))
+                            simplifySubtopicList.Add(title);
+
+                    Pair<string, string[]> pair = new Pair<string, string[]>(block, simplifySubtopicList.ToArray());
+                    blocksWithSubtopic.Add(pair);
+                }
+            }
+            catch (Exception)
+            {
+                //do nothing
+            }
+            return blocksWithSubtopic.ToArray();
+        }
+    }
+}
